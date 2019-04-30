3560c1783bb8e3379a754a7927fe72f2672720c4
Zach Banducci <zbducci@gmail.com>
2019-04-30 13:07:59 -0500

    Removed boilerplate code

--- lib/hunter_ex.ex
+++ lib/hunter_ex.ex
@@ -1,17 +1,4 @@
 defmodule HunterEx do
-  @moduledoc """
-  Documentation for HunterEx.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> HunterEx.hello()
-      :world
-
-  """
   def hello do
     :world
   end
